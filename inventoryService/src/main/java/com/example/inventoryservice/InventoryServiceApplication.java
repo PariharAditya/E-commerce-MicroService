e390e44526443d61e0ae761cc9a09b4484c15a97
Aditya Parihar <adityaparihar5050@gmail.com>
2024-04-14 14:32:22 +0530

    Notification Service ✅ dockerization []

--- inventoryService/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ inventoryService/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -5,9 +5,11 @@ import com.example.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
@@ -20,7 +22,7 @@ public class InventoryServiceApplication {
         return args -> {
             InventoryData inventoryModel = new InventoryData();
             inventoryModel.setSkuCode("phone13");
-            inventoryModel.setQuantity(100);
+            inventoryModel.setQuantity(0);
             inventoryRepository.save(inventoryModel);
 
             InventoryData inventoryModel1 = new InventoryData();
